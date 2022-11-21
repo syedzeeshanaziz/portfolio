51df9127a0d026fbe9e3fa50999c03a10ab89777
wasimarshad <wasim.arshad@teemuno.com>
2022-11-21 19:29:48 +0500

    site

---
+++ person.php
@@ -0,0 +1,35 @@
+<script type="application/ld+json">
+{
+	"@context": "http://schema.org/",
+	"@type": "Person",
+	"url":"https://zeeshanaziz.com/",
+	"image": "https://zeeshanaziz.com/images/me.jpg",
+	"name": "Zeeshan Aziz",
+	"additionalName": "Zeeshan",
+	"email":"zeeshan.aziz@urdupoint.com",
+	"telephone":"+92-300-2500052",
+	"jobTitle": "Digital Media Journalist",
+	"birthDate": "25 April 1989",
+	"gender":"Male",
+	"knowsAbout":"Digital Journalism, YouTube Content Experties, YouTube Audience Growth, YouTube Revenue Experties",
+	"knowsLanguage":"English, Urdu, Saraiki, Punjabi",
+	"memberOf":"Zeeshan Aziz is working in Pakistan's first and largest digital media UrduPoint from last 7 years as senior Journalist and Group Manager. He is associate member at Lahore Press Club along member of Provisional Assembly of the Punjab",
+	
+	"sameAs":["https://www.facebook.com/imziishan", "https://twitter.com/imziishan", "https://www.instagram.com/imziishan/", "https://www.linkedin.com/in/imziishan/"],
+	"nationality":{
+		"@type": "Country",
+		"name": "Pakistan"
+	},
+	"birthPlace":{
+		"@type": "Place",
+		"name": "Multan"
+	},
+	
+	"address":{
+		"@type": "PostalAddress",
+		"name": "55 sunflower j1 block johar town lahore"
+	}
+	
+	
+}
+</script>
